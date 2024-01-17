07a51c63110c4fa236d640f3dfd247530777953e
Michel2406 <116920125+Michel2406@users.noreply.github.com>
2024-01-17 11:44:22 +0100

    Update writeData.c

--- writeData.c
+++ writeData.c
@@ -1,31 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "writeData.h"
+#include <stdio.h>
+#include <string.h>
+#include <json-c/json.h>
 
-// Structure pour représenter les données à écrire dans le fichier JSON
-typedef struct {
-    double latitude;
-    double longitude;
-    char couleurFeu[20];  // Choisir la taille en fonction de vos besoins
-} JsonData;
+// Fonction pour mettre à jour la couleur du feu dans le fichier JSON
+void updateColorData(FILE *file, const JsonData *jsonData) {
+    fprintf(file, "{\"couleurFeu\":\"%s\"}\n", jsonData->couleurFeu);
+}
+
+// Fonction pour mettre à jour la localisation GPS dans le fichier JSON
+void updateGPSData(FILE *file, const JsonData *jsonData) {
+    fprintf(file, "{\"latitude\":%f,\"longitude\":%f}\n", jsonData->latitude, jsonData->longitude);
+}
 
 void writeData(const char *filename, const char *updateType, const JsonData *jsonData) {
-    // Ouverture du fichier en mode d'écriture
     FILE *file = fopen(filename, "w");
     if (file != NULL) {
-        // Détermination du type de mise à jour
+        // Initialisation de la structure de données JSON
+        JsonData defaultData;
+        memset(&defaultData, 0, sizeof(JsonData));
+
+        // Utilisation des fonctions appropriées en fonction du type de mise à jour
         if (strcmp(updateType, "feu") == 0) {
-            // Écriture des données pour la mise à jour de la couleur du feu
-            fprintf(file, "{\"couleurFeu\":\"%s\"}\n", jsonData->couleurFeu);
+            updateColorData(file, jsonData);
         } else if (strcmp(updateType, "gps") == 0) {
-            // Écriture des données pour la mise à jour de la localisation GPS
-            fprintf(file, "{\"latitude\":%f,\"longitude\":%f}\n", jsonData->latitude, jsonData->longitude);
+            updateGPSData(file, jsonData);
         } else {
             fprintf(stderr, "Type de mise à jour non reconnu.\n");
+            // Utilisez la fonction appropriée pour une mise à jour par défaut
+            updateGPSData(file, &defaultData);
         }
 
-        // Fermeture du fichier
         fclose(file);
     } else {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier %s pour écriture.\n", filename);
