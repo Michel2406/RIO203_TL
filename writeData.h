dbdaa20931b37aed355d868ec02b2fb6bbe66e4e
Michel2406 <116920125+Michel2406@users.noreply.github.com>
2024-01-17 11:44:43 +0100

    Update writeData.h

--- writeData.h
+++ writeData.h
@@ -1,6 +1,13 @@
-#ifndef WRITE_H
-#define WRITE_H
+#ifndef WRITEDATA_H
+#define WRITEDATA_H
+
+typedef struct {
+    double latitude;
+    double longitude;
+    char couleurFeu[20];
+} JsonData;
 
 void writeData(const char *filename, const char *updateType, const JsonData *jsonData);
 
-#endif
+#endif // WRITEDATA_H
+
