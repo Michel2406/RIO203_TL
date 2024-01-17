fc874454a18108cd52200db8d51ddb58f7fbd12e
Michel2406 <116920125+Michel2406@users.noreply.github.com>
2024-01-17 09:52:43 +0100

    Update writeData.c

--- writeData.c
+++ writeData.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-int writeData(char * filename, char * data) {
+int writeData(char *filename, char *data) {
     // Ouvrir un fichier en écriture
-    int file_descriptor = open(filename, O_CREAT | O_WRONLY |O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1) {
+    FILE *file = fopen(filename, "a");
+    if (file == NULL) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(EXIT_FAILURE);
     }
-    // Écrire dans le fichier
-    ssize_t bytes_ecrits = write(file_descriptor, data, strlen(data));
 
-    if (bytes_ecrits == -1) {
+    // Écrire dans le fichier
+    size_t bytes_ecrits = fwrite(data, 1, strlen(data), file);
+
+    if (bytes_ecrits != strlen(data)) {
         perror("Erreur lors de l'écriture dans le fichier");
-        close(file_descriptor);
+        fclose(file);
         exit(EXIT_FAILURE);
     }
+
     // Fermer le fichier après l'écriture
-    close(file_descriptor);
+    fclose(file);
 
     return 0;
 }
+
