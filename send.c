4c1eb36605179a082c65c84f430a8c4181da0a38
Michel2406 <116920125+Michel2406@users.noreply.github.com>
2024-01-17 11:26:13 +0100

    Update send.c

--- send.c
+++ send.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <curl/curl.h>
 
-void performPostRequest(CURL *curl, const char *url, const char *postData);
+size_t read_callback(void *ptr, size_t size, size_t nmemb, void *userdata);
 
 int main(void) {
     CURL *curl;
@@ -14,45 +12,21 @@ int main(void) {
     // Création de l'objet CURL
     curl = curl_easy_init();
     if(curl) {
-        while(1){
-            // Lecture du contenu du fichier data.json
-            FILE *file = fopen("data.json", "r");
-            if (!file) {
-                fprintf(stderr, "Erreur lors de l'ouverture du fichier data.json\n");
-                return 1;
-            }
-
-            // Obtention de la taille du fichier
-            fseek(file, 0, SEEK_END);
-            long size = ftell(file);
-            fseek(file, 0, SEEK_SET);
-
-            // Allocation d'un tampon pour stocker le contenu du fichier
-            char *postData = (char *)malloc(size + 1);
-            if (!postData) {
-                fprintf(stderr, "Erreur lors de l'allocation de mémoire\n");
-                fclose(file);
-                return 1;
-            }
-
-            // Lecture du contenu du fichier dans le tampon
-            fread(postData, 1, size, file);
-            postData[size] = '\0';  // Ajout d'un caractère de fin de chaîne
-
-            // Fermeture du fichier
-            fclose(file);
-
-            // Appel de la fonction pour effectuer la requête POST
-            printf("Sending\n");
-            performPostRequest(curl, "http://192.168.251.55:9090/api/v1/zEKH1U9te4RBoOeFwwUU/telemetry", postData);
-            printf("Sent\n");
-            // Libération de la mémoire allouée pour le tampon
-            free(postData);
-
-            // Libération des ressources CURL
-            curl_easy_cleanup(curl);
-            sleep(1);
+        // Ouverture du fichier data.json
+        FILE *file = fopen("data.json", "r");
+        if (!file) {
+            fprintf(stderr, "Erreur lors de l'ouverture du fichier data.json\n");
+            return 1;
         }
+
+        // Appel de la fonction pour effectuer la requête POST avec le contenu du fichier
+        performPostRequest(curl, "http://192.168.251.55:9090/api/v1/zEKH1U9te4RBoOeFwwUU/telemetry", file);
+
+        // Fermeture du fichier
+        fclose(file);
+
+        // Libération des ressources CURL
+        curl_easy_cleanup(curl);
     }
 
     // Libération des ressources globales de libcurl
@@ -61,8 +35,8 @@ int main(void) {
     return 0;
 }
 
-// Fonction pour effectuer une requête POST avec CURL
-void performPostRequest(CURL *curl, const char *url, const char *postData) {
+// Fonction pour effectuer une requête POST avec CURL en utilisant le contenu du fichier
+void performPostRequest(CURL *curl, const char *url, FILE *file) {
     CURLcode res;
 
     // URL de la requête POST
@@ -71,8 +45,9 @@ void performPostRequest(CURL *curl, const char *url, const char *postData) {
     // Spécification du type de requête (POST)
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
 
-    // Spécification du contenu JSON
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData);
+    // Spécification de la fonction de lecture du fichier
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+    curl_easy_setopt(curl, CURLOPT_READDATA, file);
 
     // En-tête Content-Type
     struct curl_slist *headers = NULL;
@@ -88,4 +63,11 @@ void performPostRequest(CURL *curl, const char *url, const char *postData) {
 
     // Libération des en-têtes
     curl_slist_free_all(headers);
+}
+
+// Fonction de rappel de lecture pour CURLOPT_READFUNCTION
+size_t read_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
+    FILE *file = (FILE *)userdata;
+    size_t retcode = fread(ptr, size, nmemb, file);
+    return retcode;
 }
