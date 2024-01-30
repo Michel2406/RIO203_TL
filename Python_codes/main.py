87d0d6c5fe230d0fd03b6bc77c9823c2a7c46a83
Samuel Oussou <samueloussou2016@gmail.com>
2024-01-30 04:17:53 +0100

    ok

--- Python_codes/main.py
+++ Python_codes/main.py
@@ -1,23 +1,22 @@
 import time
-from write_data import read_json, insert_gps_leds_data, insert_led_data
+import send
 
 def main():
-    gps_leds_data_file = "gps_leds_data.json"
-    led_data_file = "led_data.json"
-
     while True:
         # Lire les données depuis les fichiers JSON
-        gps_leds_data = read_json(gps_leds_data_file)
-        led_data = read_json(led_data_file)
+        gps_leds_data = send.read_json("../gps.json")
+        led_data = send.read_json("../color.json")
 
         # Insérer les données dans la base de données
         if gps_leds_data:
-            insert_gps_leds_data(gps_leds_data)
+            print(gps_leds_data)
+            #send.insert_gps_leds_data(gps_leds_data)
         if led_data:
-            insert_led_data(led_data)
+            print(led_data)
+            #send.insert_led_data(led_data)
 
         # Attendre pendant un certain temps avant la prochaine itération
-        time.sleep(5)  # Attendre 5 secondes (ajustez selon vos besoins)
+        time.sleep(3)  # Attendre 5 secondes (ajustez selon vos besoins)
 
 if __name__ == "__main__":
     main()
--- Python_codes/send.py
+++ Python_codes/send.py
@@ -1,13 +1,11 @@
 import mysql.connector
 import json
 
-
-
 def read_json(nom_fichier):
     try:
         with open(nom_fichier, 'r') as fichier:
             donnees_json = json.load(fichier)
-            return donnees_json[0]
+            return donnees_json
     except FileNotFoundError:
         print(f"Le fichier {nom_fichier} n'a pas été trouvé.")
         return None
@@ -42,4 +40,29 @@ def insert_led_data(led_data):
     conn.close()
 
 # Exemple d'utilisation de la fonction avec un objet JSON
+
+def insert_gps_leds_data(gps_leds_data):
+    # Paramètres de connexion à la base de données MySQL
+    conn_params = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'rio203',
+        'database': 'rio',
+        'auth_plugin': 'rio203'
+    }
+
+    # Connexion à la base de données
+    conn = mysql.connector.connect(**conn_params)
+    cursor = conn.cursor()
+
+    # Insertion de données dans la table "GPS_leds"
+    insert_query = "INSERT INTO GPS_leds (latitude, longitude) VALUES (%s, %s)"
+    cursor.execute(insert_query, (gps_leds_data['latitude'], gps_leds_data['longitude']))
+
+    # Commit des modifications
+    conn.commit()
+
+    # Fermeture de la connexion
+    cursor.close()
+    conn.close()
 
