c4b0c9f565b372163a37c6b06b24f8c39e4b5cac
Samuel Oussou <samueloussou2016@gmail.com>
2024-01-30 19:07:56 +0100

    modif

--- Python_codes/receive.py
+++ Python_codes/receive.py
@@ -4,7 +4,7 @@ import json
 
 # Paramètres de connexion à la base de données MySQL
 conn_params = {
-    'host': '192.168.96.55',
+    'host': '192.168.126.55',
     'user': 'root',
     'password': 'rio203',
     'database': 'rio',
@@ -22,18 +22,13 @@ def check_attribute_value_and_write_to_json():
     # Récupérer toutes les lignes
     results = cursor.fetchall()
 
-    # Construire une liste de dictionnaires pour chaque ligne
-    data_to_write = []
-    for result in results:
-        data_to_write.append({'ID': result[0], 'demand': result[1]})
-
     # Fermeture de la connexion
     cursor.close()
     conn.close()
 
     # Écrire les données dans un fichier JSON
-    with open('output.json', 'w') as json_file:
-        json.dump(data_to_write, json_file, indent=2)
+    with open('output.txt', 'w') as file:
+        
 
 # Boucle infinie pour vérifier périodiquement la valeur de l'attribut et écrire dans un fichier JSON
 while True:
